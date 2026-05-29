2200e1a0a424f31baf5942d01854bdc76c311b30
[k8redfern] <[x2023crd@stfx.ca]>
2026-05-29 09:12:07 -0300

    Style(conf.py): amend the website style
    Set the website's favicon and site logo. Specified the Sphinx theme in the
    extension list. Set date formatting to ISO8601 standard of YYYY-MM-DD. Also
    added a global variable for substituting the update month to include an
    update log on each page, with a link to the ISO8601 standards page to define
    the date for users of the website.
    
    ISO8601 Link: https://www.iso.org/iso-8601-date-and-time-format.html

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -3,6 +3,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+from datetime import datetime.today()
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -14,7 +16,12 @@ release = '0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+html_logo = 'icons/BrainHealthLabLogo.png'
+html_favicon = 'icons/mag_brain.ico'
+
+today = '%Y-%b-%d'
+
+extensions = ['sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -26,3 +33,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+
+# -- Options for Markup ------------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-markup
+
+rst_prolog = """
+.. | update_month | replace:: {datetime.today()}
+.. _`update_month`: https://www.iso.org/iso-8601-date-and-time-format.html
+"""
