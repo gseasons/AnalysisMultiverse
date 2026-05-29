30698a37769fa9e26892ab8bd73a0e8080a90b82
[k8redfern] <[x2023crd@stfx.ca]>
2026-05-29 12:33:02 -0300

    Build(conf.py): add markdown files
    Added the recommended source_suffix dictionary to the conf.py file under
    general configuration. This is to add support for the markdown documentation
    files to be used on the documentation website.
    
    Reference: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-source_suffix

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -26,7 +26,11 @@ extensions = ['sphinx_rtd_theme', 'myst_parser']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'restructuredtext',
+    '.md': 'markdown',
+    }
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
